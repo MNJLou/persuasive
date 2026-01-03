8435281429520435e5c7f3b7149c09b296aca8ea
MNJLou <jeromelou502@gmail.com>
2026-01-03 22:38:16 +0200

    Fixed naming

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/Persuasive_Website",
+    base: process.env.VITE_BASE_PATH || "/persuasive_website",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
