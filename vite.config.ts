0c5f759fc4b69b7f53270e942297736ca25d01c4
MNJLou <jeromelou502@gmail.com>
2026-01-03 23:10:01 +0200

    Added dist 1.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/persuasive_website",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
